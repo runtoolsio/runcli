14c33a13e5f065962126f0adb086452f773e9118
stan <stan@taggstar.com>
2025-04-20 13:30:10 +0700

    instance ID parameter in `create` function

--- src/runtools/runcli/job.py
+++ src/runtools/runcli/job.py
@@ -1,9 +1,11 @@
+from runtools.runcore.job import InstanceID
 from runtools.runjob import environment
 from runtools.runjob.program import ProgramPhase
 
 
 def run(args):
     job_id = args.id or " ".join([args.command.removeprefix('./')] + args.arg)
+    instance_id = InstanceID(job_id)
     phase = ProgramPhase('PROGRAM', *([args.command] + args.arg))
     with environment.local() as env:
-        env.create_instance(job_id, [phase]).run()
+        env.create_instance(instance_id, [phase]).run()
