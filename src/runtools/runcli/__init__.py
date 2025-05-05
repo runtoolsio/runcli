cbac0297d721106048f0ed2d255282b55ff0ca3d
stan <stan@taggstar.com>
2025-05-05 23:07:13 +0700

    default environment only in runcli

--- src/runtools/runcli/__init__.py
+++ src/runtools/runcli/__init__.py
@@ -3,19 +3,18 @@ This is a command line interface for the `runjob` library.
 """
 import logging
 
-import sys
+from rich.console import Console
+from rich.text import Text
 
-from runtools.runcore import util
-from runtools.runcore.env import DEFAULT_ENVIRONMENT
+from runtools.runcore import util, env
+from runtools.runcore.env import DEFAULT_LOCAL_ENVIRONMENT, EnvironmentNotFoundError, EnvironmentConfigUnion
 from runtools.runcore.err import RuntoolsException
+from runtools.runcore.job import InstanceID
 from runtools.runcore.paths import ConfigFileNotFoundError
 from runtools.runcore.util import update_nested_dict
 from . import __version__, cmd, cli, log, job
 from .cfg import CONFIG_FILE
 from .cli import ACTION_CONFIG
-
-from rich.console import Console
-from rich.text import Text
 
 logger = logging.getLogger(__name__)
 
@@ -37,13 +36,8 @@ def main(args):
     """
     try:
         run_app(args)
-    except ConfigFileNotFoundError as e:
-        print("User error: " + str(e), file=sys.stderr)
-        if e.search_path:
-            print("Run `config create` command to create configuration file "
-                  "or use `-dc` option to execute using default config", file=sys.stderr)
-        exit(1)
     except RuntoolsException as e:
+        # TODO print to stderr
         console.print(Text().append("User error: ", style="bold red").append(str(e)))
         exit(1)
     except KeyboardInterrupt:
@@ -56,7 +50,7 @@ def run_app(args):
     if args_parsed.action == ACTION_CONFIG:
         run_config(args_parsed)
     else:
-        run_command(args_parsed)
+        run_job(args_parsed)
 
 
 def run_config(args):
@@ -69,7 +63,16 @@ def run_config(args):
         print("Created " + str(cfg.create_config_file(getattr(args, 'path'), overwrite=args.overwrite)))
 
 
-def run_command(args):
+def run_job(args):
+    job_id = args.id or " ".join([args.command.removeprefix('./')] + args.arg)
+    instance_id = InstanceID(job_id, getattr(args, 'run_id'))
+    config = load_config(instance_id, args)
+    env_config = get_env_config(args, config, instance_id)
+
+    job.run(instance_id, env_config, args)
+
+
+def load_config(instance_id, args):
     cfg_found = True
     if getattr(args, 'def_config', False):
         config, cfg_path = cfg.read_default_configuration()
@@ -85,8 +88,36 @@ def run_command(args):
                 else:
                     cfg_found = False
                     config, cfg_path = cfg.read_default_configuration()
-    update_nested_dict(config, util.split_params(args.set))  # Override config by `set` args
 
+    update_nested_dict(config, util.split_params(args.set))  # Override config by `set` args
+    configure_logging(config)
+
+    if cfg_found:
+        logger.info(f"[configuration_loaded] instance=[{instance_id}] source=[{cfg_path}]")
+    else:
+        logger.warning(f"[fallback_configuration_loaded] instance=[{instance_id}] fallback_source=[{cfg_path}] reason=[config_file_not_found]")
+
+    return config
+
+
+def get_env_config(args, config, instance_id) -> EnvironmentConfigUnion:
+    def_env_id = config.get('environments', {'default': DEFAULT_LOCAL_ENVIRONMENT}).get('default', DEFAULT_LOCAL_ENVIRONMENT)
+    env_id = getattr(args, 'env') or def_env_id
+    try:
+        env_config, env_config_path = env.load_env_config(env_id)
+        logger.info(f"[environment_config_loaded] instance=[{instance_id}] env=[{env_id}] path=[{env_config_path}]")
+    except (ConfigFileNotFoundError, EnvironmentNotFoundError) as e:
+        if getattr(args, 'config_required', False) or env_id != DEFAULT_LOCAL_ENVIRONMENT:
+            logger.error(f"[environment_config_not_found] instance=[{instance_id}] env=[{env_id}]")
+            raise e
+        env_config, env_config_path = env.load_env_default_config(env_id)
+        logger.info(f"[environment_default_config_loaded] instance=[{instance_id}] env=[{env_id}] path=[{env_config_path}] "
+                    f"reason=[No config for `{env_id}` env found]")
+
+    return env.env_config_from_dict(env_config)
+
+
+def configure_logging(config):
     log_config = config.get('log', {})
     log.configure(
         log_config.get('enabled', True),
@@ -94,10 +125,3 @@ def run_command(args):
         log_config.get('file', {}).get('level', 'info'),
         log_config.get('file', {}).get('path', None),
     )
-    if cfg_found:
-        logger.info(f"[configuration_loaded] source=[{cfg_path}]")
-    else:
-        logger.warning(f"[fallback_configuration_loaded] fallback_source=[{cfg_path}] reason=[config_file_not_found]")
-
-    def_env = config.get('environments', {'default': DEFAULT_ENVIRONMENT}).get('default', DEFAULT_ENVIRONMENT)
-    job.run(def_env, args)
--- src/runtools/runcli/job.py
+++ src/runtools/runcli/job.py
@@ -1,17 +1,12 @@
-from runtools.runcore import env
-from runtools.runcore.env import DEFAULT_ENVIRONMENT
-from runtools.runcore.job import InstanceID
 from runtools.runjob import node
 from runtools.runjob.coord import MutualExclusionPhase
 from runtools.runjob.program import ProgramPhase
 
 
-def run(def_env, args):
-    env_config = env.get_env_config(getattr(args, 'env') or def_env, fallback_default=(def_env == DEFAULT_ENVIRONMENT))
-    job_id = args.id or " ".join([args.command.removeprefix('./')] + args.arg)
-    instance_id = InstanceID(job_id, getattr(args, 'run_id'))
+def run(instance_id, env_config, args):
+    # TODO Log term status
     with node.create(env_config) as env_node:
-        env_node.create_instance(instance_id, create_phases(job_id, args)).run()
+        env_node.create_instance(instance_id, create_phases(instance_id.job_id, args)).run()
 
 
 def create_phases(job_id, args):
