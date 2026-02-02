49e5bf8392c9ccd4cd0a4af460e67534286f71da
stan <stan@taggstar.com>
2026-02-02 23:45:35 +0700

    moved clean stale sockets function to appropriate module

--- src/runtools/runcli/cmd/clean.py
+++ src/runtools/runcli/cmd/clean.py
@@ -1,18 +1,18 @@
-from runtools import runner
 from runtools.runcore import paths
-
-from runtools.runner import api, events
+from runtools.runcore.listening import TRANSITION_LISTENER_FILE_EXTENSION, OUTPUT_LISTENER_FILE_EXTENSION
+from runtools.runcore.util.socket import clean_stale_sockets
+from runtools.runjob.server import RPC_FILE_EXTENSION
 
 
 def run(args):
     # TODO Disable socket log messages
-    clean_socket(api.API_FILE_EXTENSION, "API")
-    clean_socket(events.TRANSITION_LISTENER_FILE_EXTENSION, "transition listeners")
-    clean_socket(events.OUTPUT_LISTENER_FILE_EXTENSION, "output listeners")
+    clean_socket(RPC_FILE_EXTENSION, "RPC")
+    clean_socket(TRANSITION_LISTENER_FILE_EXTENSION, "transition listeners")
+    clean_socket(OUTPUT_LISTENER_FILE_EXTENSION, "output listeners")
 
 
 def clean_socket(file_extension, socket_group):
-    cleaned = runner.clean_stale_sockets(paths.socket_files_provider(file_extension))
+    cleaned = clean_stale_sockets(paths.socket_files_provider(file_extension))
     print(f"Cleaned sockets for {socket_group}: {len(cleaned)}")
     for c in cleaned:
         print('  cleaned: ' + str(c))
